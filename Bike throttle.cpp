b9405aed0d3ea816b4a77fafb69837b0f84a298d
AllanGamal <63316423+AllanGamal@users.noreply.github.com>
2021-07-16 17:53:42 +0200

    added magnet function

--- Bike throttle.cpp
+++ Bike throttle.cpp
@@ -1,7 +1,7 @@
 
 int throttle = A0;
-int halls = A5;
-int data = 0;
+int Hall = 13;
+int data = 170;
 int datah = 0;
 
 #include <Servo.h>
@@ -12,51 +12,47 @@ const uint8_t PotentiometerPin = A0;
 unsigned int tempPin = PotentiometerPin;
 Servo esc;
 
-//
 volatile unsigned int count = 0;
 unsigned long next = 100;
 unsigned int temp = 0;
 
 void setup()
 {
+  pinMode(Hall, INPUT);
   Serial.begin(9600);
-  pinMode(halls, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), magnet_detect, RISING);
 
   esc.attach(VescOutputPin);
 
   esc.writeMicroseconds(1500);
-
-  //
-
-  attachInterrupt(digitalPinToInterrupt(2), magnet_detect, RISING);
 }
 
 void loop()
 {
-  data = analogRead(tempPin);
-  datah = analogRead(halls);
-  //Serial.print("Sensor data:");
-  if (count > 1)
+  // Set throttle at 0 (170)
+  data = 170;
+
+  // Make throttle available when hall sensor sensor magnets/pedeling
+  if (temp > 1 || digitalRead(Hall) == 0)
   {
-    Serial.println(data);
-    esc.writeMicroseconds(map(analogRead(PotentiometerPin), 0, 1023, 1000, 2000));
+    tempPin = PotentiometerPin;
+    data = analogRead(tempPin);
   }
   else
   {
-
     tempPin = 170;
-    Serial.println(tempPin);
   }
+  //Serial.println(digitalRead(Hall));
+  //data = analogRead(tempPin);
+  Serial.println(data);
+  esc.writeMicroseconds(map(analogRead(data), 0, 1023, 1000, 2000));
 
-  //Serial.println(datah);
-
-  //
+  // Count magnet passes every 500 ms
   if (millis() > next)
   {
     temp = count;
-    //Serial.println(count);
     count -= temp;
-    next += 1500;
+    next += 500;
   }
 }
 
