11e8c9e205ef51be02ad75945e5421478af15e8e
AllanGamal <63316423+AllanGamal@users.noreply.github.com>
2021-07-31 20:41:08 +0200

    Risky update
    Fixing the delay of motor activation when pedeling

--- Bike throttle.cpp
+++ Bike throttle.cpp
@@ -1,13 +1,15 @@
-
 int throttle = A0;
 int Hall = 13;
 int data = 170;
-int datah = 0;
+
+// Interval is how long we wait
+int interval = 1000;
+// Tracks the time since last event fired
+unsigned long previousMillis = 0;
 
 #include <Servo.h>
 
 const uint8_t VescOutputPin = 5;
-
 const uint8_t PotentiometerPin = A0;
 unsigned int tempPin = PotentiometerPin;
 Servo esc;
@@ -23,26 +25,55 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(2), magnet_detect, RISING);
 
   esc.attach(VescOutputPin);
-
   esc.writeMicroseconds(1500);
 }
 
 void loop()
 {
-  // Set throttle at 0 (170)
-  data = 170;
 
+  // Set throttle at 0 (170)
+  data = 160;
+
+  // Get snapshot of time
+  unsigned long currentMillis = millis();
+  if (temp > 0)
+  {
+    // How much time has passed, accounting for rollover with subtraction!
+    if ((unsigned long)(currentMillis - previousMillis) >= interval)
+    {
+      // It's time to do something!
+
+      // Use the snapshot to set track time until next event
+      previousMillis = currentMillis;
+    }
+
+    if (temp != count)
+    {
+      previousMillis = currentMillis;
+    }
+  }
+
+  unsigned long throttleInterval = currentMillis - previousMillis;
+  if (throttleInterval > interval)
+  {
+    throttleInterval = interval;
+  }
+  //Serial.println(throttleInterval);
+  //Serial.println(digitalRead(Hall));
+
+  ///////////////////////////////////////
   // Make throttle available when hall sensor sensor magnets/pedeling
-  if (temp > 1 || digitalRead(Hall) == 0)
+  if (throttleInterval < 1000)
   {
     tempPin = PotentiometerPin;
     data = analogRead(tempPin);
   }
   else
   {
-    tempPin = 170;
+    tempPin = 160;
   }
-  //Serial.println(digitalRead(Hall));
+  //////////////////////////////////////
+
   //data = analogRead(tempPin);
   Serial.println(data);
   esc.writeMicroseconds(map(analogRead(data), 0, 1023, 1000, 2000));
@@ -52,8 +83,9 @@ void loop()
   {
     temp = count;
     count -= temp;
-    next += 500;
+    //next += 500;
   }
+  //Serial.println(temp);
 }
 
 void magnet_detect()
